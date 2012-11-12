74e6516ff0e6d0ce7ecaac5187f9894b33fb2129
Dino Petrone <dinopetrone@gmail.com>
2012-11-12 07:16:14 -0800

    adding gunicorn to base and cleaning up ignore

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,4 +1,4 @@
-from fabric.api import local, cd, get, env, roles, execute, task, run
+from fabric.api import local, cd, get, env, roles, execute, task, run, settings
 
 env.roledefs       = {
                       'vagrant'                : ['vagrant@127.0.0.1:2222']
@@ -10,9 +10,20 @@ env.passwords      = {
 @task
 @roles('vagrant')
 def runserver():
-	run("ps ax | grep [p]ython | awk '{ print $1 }' | xargs kill -9")
+	with settings(warn_only=True):
+		# FOR SOME REASON IF THE PROCESS WASN'T ENDED CORRECTLY, THIS WILL KILL IT
+		run("ps ax | grep [p]ython | awk '{ print $1 }' | xargs kill -9")
 	with cd("/var/www/project"):
 		run('python ./manage.py runserver [::]:8000')
+
+@task
+@roles('vagrant')
+def gunicorn():
+	with settings(warn_only=True):
+		# FOR SOME REASON IF THE PROCESS WASN'T ENDED CORRECTLY, THIS WILL KILL IT
+		run("ps ax | grep [p]ython | awk '{ print $1 }' | xargs kill -9")
+	with cd("/var/www/project"):
+		run('python ./manage.py run_gunicorn [::]:8000')
 
 @task
 @roles('vagrant')		
--- project/settings/base.py
+++ project/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = (
 
     'require',
     #'debug-toolbar',
+    'gunicorn',
     'tastypie',
     
 )
