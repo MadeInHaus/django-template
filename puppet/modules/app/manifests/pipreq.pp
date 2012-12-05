c76b556c94871c6271f3cf592a420cecefa5cc4e
Joshua Thorp <joshua@stigmergic.net>
2012-12-05 12:13:43 -0700

    giving libevent its own pp file

--- puppet/modules/app/manifests/pipreq.pp
+++ puppet/modules/app/manifests/pipreq.pp
@@ -1,9 +1,5 @@
 class app::pipreq {
 
-    package{['libevent-dev']:
-            ensure => installed
-    }
-    
     exec{'pip-install':
     	require => Package['python-pip','postgresql','libpq-dev','rabbitmq-server','redis-server','memcached','libevent-dev'],
         command => "sudo pip install -r /var/www/requirements.txt",
