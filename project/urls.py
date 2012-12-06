358fc619d120b0b807c4d38d7b8918a2e8e24cb2
Joshua Thorp <joshua@stigmergic.net>
2012-12-06 14:11:58 -0700

    adding a default index.html template

--- project/urls.py
+++ project/urls.py
@@ -12,7 +12,12 @@ urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': False,}),
 
+    # Homepage
+    (r'^$', TemplateView.as_view(template_name='index.html')),
 )
 
+if getattr(settings, 'SERVE_STATIC', False) and settings.SERVE_STATIC:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': False,}),
+    )
