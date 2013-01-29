d5fab98a5e27951155096d71212f07dff8b875fd
Joshua Thorp <joshua@stigmergic.net>
2013-01-29 12:23:27 -0700

    resetdb should be available by default

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,6 +1,6 @@
 from fabric.api import env
 import vagrant
-from vagrant import runall,killall
+from vagrant import runall,killall,resetdb
 
 env.roledefs       = {
                       'vagrant'                : ['vagrant@127.0.0.1:2222']
