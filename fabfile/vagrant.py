2f1daf444e4e4dfa6cb7a1dc566a689fe5858eb6
Dino Petrone <dino@madeinhaus.com>
2013-05-17 12:01:20 -0700

    fixing vagrant fab scripts

--- fabfile/vagrant.py
+++ fabfile/vagrant.py
@@ -1,17 +1,30 @@
+from os import path
 from subprocess import Popen
-from fabric.api import local, cd, get, env, roles, execute, task, run, settings
+from fabric.api import local, cd, lcd, get, env, roles, execute, task, run, \
+                       settings, abort, hide
 from fabric.colors import yellow
 
 
 # paths
-base_path           = "./project/static/css"
-src_path   = base_path + "/src"
-css_path    = base_path + "/"
+base_path   = "./project/static"
+css_path    = base_path + "/css/"
+config_path = css_path + "config.rb"
+
 
 # sass execs
 exec_sass_watch   = "compass watch {} -c {}"
+#exec_sass_compile = "compass compile {} --output-style compressed -c {} --force"
 exec_sass_compile = "compass compile {} -c {} --trace --force"
 
+# Check if current VM belongs to this project
+with hide('everything'):
+    root_dir = path.abspath(path.join(path.dirname(__file__), '..'))
+    readme_filename = path.join(root_dir, 'README.md')
+    local_readme  = local("md5 %s | awk '{ print $4 }'" % (readme_filename,), capture=True)
+    remote_readme = local("vagrant ssh -c 'md5sum /var/www/README.md' | awk '{ print $1 }'", capture=True)
+
+if remote_readme != local_readme:
+    abort('Different VM running')
 
 @task
 def runall():
@@ -30,6 +43,7 @@ def killall():
         run("ps ax | grep [r]un_gunicorn | awk '{ print $1 }' | xargs sudo kill -9")
         run("ps ax | grep [w]orker | awk '{ print $1 }' | xargs sudo kill -9")
         run("ps ax | grep [c]elerybeat | awk '{ print $1 }' | xargs sudo kill -9")
+        run("ps ax | grep [c]ompass | awk '{ print $1 }' | xargs sudo kill -9")
         run("ps ax | grep [s]ass | awk '{ print $1 }' | xargs sudo kill -9")
 
 @task
@@ -71,11 +85,14 @@ def celerybeat():
 
 @task
 @roles('vagrant')
-def initdb():
+def initdb(load_images=False):
     with cd("/var/www"):
         run('yes no | python manage.py syncdb')
         run('python manage.py migrate')
         run('python manage.py createsuperuser')
+
+    if load_images:
+        load_fixture_images()
 
 
 @task
@@ -87,8 +104,15 @@ def syncdb():
 
 @task
 @roles('vagrant')
-def resetdb():
-    execute('killall')
+def resetall():
+    """Stop all services, destroy the database, restore it from fixtures, remove all files in uploads directory and download assets."""
+    killall()
+    local('vagrant provision')
+    resetdb(delete_images=True, load_images=True)
+
+@task
+@roles('vagrant')
+def resetdb(load_images=False, delete_images=False):
     # mysql
     #run("mysql -u vagrant -pvagrant -e 'drop database if exists django'")
     #run('mysql -u vagrant -pvagrant -e "create database django"')
@@ -96,14 +120,28 @@ def resetdb():
     # postgres
     run('dropdb django')
     run('createdb django')
-    initdb()
 
+    if delete_images:
+        with cd("/var/www/uploads"):
+            run('rm -rf ./*')
+
+    initdb(load_images)
+
+@task
+def load_fixture_images():
+    uploads_dir = path.abspath(path.join(path.dirname(__file__), '../uploads'))
+    with lcd(uploads_dir):
+        with settings(warn_only=True):
+            local('rm -rf ./*')
+        local('curl -sLO https://dl.dropboxusercontent.com/s/zpb8troupk1x2g1/haus_com_assets.tar.bz2')
+        local('tar xjvf haus_com_assets.tar.bz2')
+        local('rm haus_com_assets.tar.bz2')
 
 @task
 @roles('vagrant')
-def collectstatic():
+def collectstatic(no_input=False):
     with cd("/var/www"):
-        run('python manage.py collectstatic')
+        run('python manage.py collectstatic {}'.format('--noinput' if no_input else ''))
 
 @task
 @roles('vagrant')
@@ -117,15 +155,15 @@ def css_watch(new_config=None):
 @task
 @roles('vagrant')
 def pipinstall():
-    run('/home/vagrant/.venv/bin/pip install -r /var/www/requirements.txt')
+    run('/home/vagrant/.venv/bin/pip install --use-mirrors -r /var/www/requirements.txt')
 
 @task
 @roles('vagrant')
 def freeze():
-    run('/home/vagrant/.venv/bin/pip freeze > /var/www/requirements.txt')
+    run('/home/vagrant/.venv/bin/pip freeze > /var/www/current-requirements.txt')
 
 @task
 @roles('vagrant')
 def css_compile():
     with cd("/var/www"):
-        run(exec_sass_compile.format(css_path))
+        run(exec_sass_compile.format(base_path, config_path))
