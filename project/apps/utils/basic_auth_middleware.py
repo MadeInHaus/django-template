aa04041cdf7b06b072a1283072446de23094c0b5
Joshua Thorp <joshua@stigmergic.net>
2013-10-10 14:14:40 -0600

    adding way to switch basic auth on or off via env var

--- project/apps/utils/basic_auth_middleware.py
+++ project/apps/utils/basic_auth_middleware.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.middleware.common import CommonMiddleware
 from django.conf import settings
 
+import os
+
 class AuthMiddleware(CommonMiddleware):
     """
 Add this to middleware:
@@ -10,14 +12,14 @@ Add this to middleware:
 
 
 Add these settings:
-USE_BASIC_AUTH = True
+USE_BASIC_AUTH = True # This setting is optionally settable as an env var,  env var will override whatever is set in settings
 BASIC_AUTH_USER = 'user'
 BASIC_AUTH_PASS = 'password'
 
 """
 
     def process_request(self, request):
-        if getattr(settings, 'USE_BASIC_AUTH', False):
+        if (getattr(settings, 'USE_BASIC_AUTH', False) or os.environ.get('USE_BASIC_AUTH', 'False')=='True') and not os.environ.get('USE_BASIC_AUTH', None)=='False':
             if request.META.get('HTTP_AUTHORIZATION', False):
                 authtype, auth = request.META['HTTP_AUTHORIZATION'].split(' ')
                 auth = base64.b64decode(auth)
