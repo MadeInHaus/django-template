79e042d8dc3113656574513f3892a878df0d061a
Joshua Thorp <joshua@stigmergic.net>
2014-01-23 11:58:24 -0700

    homepage view needs to be last since it is a catch all

--- project/urls.py
+++ project/urls.py
@@ -13,12 +13,10 @@ urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-
-    # Homepage
-    (r'^$', TemplateView.as_view(template_name='index.html')),
 )
 
-#used to show static assets out of the collected-static
+# Used to show static assets out of the collected-static and user media out of the uploads directory.
+# Note this must be turned of with SERVE_STATIC var in settings.py
 if getattr(settings, 'SERVE_STATIC', False) and settings.SERVE_STATIC:
     urlpatterns += patterns('',
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': False,}),
@@ -30,3 +28,11 @@ if getattr(settings, 'DEBUG', False):
     urlpatterns += patterns('',
                             (r'^api_profile/v1/(?P<resource>.*)/$', 'utils.api_views.api_profile'),
                             )
+
+# Homepage catchall needs to go last.
+urlpatterns += patterns('',
+    # Homepage
+    (r'^$', TemplateView.as_view(template_name='index.html')),
+)
+
+
