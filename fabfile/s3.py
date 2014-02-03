d9ece7ce23f74a0e6e01ef495bbad1445aa41444
Joshua McDonald <me@joshuamcdonald.info>
2014-02-03 15:50:42 -0500

    fab s3.setup:shortname now creates group and user as well

--- fabfile/s3.py
+++ fabfile/s3.py
@@ -1,17 +1,16 @@
 import boto
 from fabric.colors import red
 from fabric.api import task
+from time import sleep
 
 import logging
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
-@task
-def setup(shortname=None):
-	"""Setup 3 s3 buckets and tag them.  Pass shortname into task"""
-	if shortname is None:
-		log.error(red('You must provide a shortname'))
 
+
+def create_buckets(shortname):
+	
 	conn = boto.connect_s3()
 
 	buckets = (shortname, shortname + '-staging', shortname + '-dev')
@@ -27,4 +26,50 @@ def setup(shortname=None):
 		bucket.set_tags(tags)
 		bucket.make_public(True)
 
+
+policy = """{{	
+"Statement": [
+        {{
+            "Effect": "Allow",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": "arn:aws:s3:::*"
+        }},
+        {{
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::{0}-dev",
+                "arn:aws:s3:::{0}-dev/*",
+                "arn:aws:s3:::{0}-staging",
+                "arn:aws:s3:::{0}-staging/*",
+                "arn:aws:s3:::{0}-prod",
+                "arn:aws:s3:::{0}-prod/*"
+
+            ]:
+        }}
+    ]
+}}"""	
+
+def create_user(shortname=None):
+
+	conn = boto.connect_iam()
+	response = conn.create_group(shortname)
+	response = conn.put_group_policy(shortname, shortname + 'Policy', policy.format(shortname))
 	
+	# make the user
+	response = conn.create_user(shortname)
+	response = conn.add_user_to_group(shortname, shortname)
+	response = conn.create_access_key(shortname)
+
+	sleep(5) # give the access key a chance to propogate
+	create_buckets(shortname=shortname)
+
+@task
+def setup(shortname=None):
+	"""Setup 3 s3 buckets and tag them.  Pass shortname into task"""
+	if shortname is None:
+		log.error(red('You must provide a shortname'))
+		return
+
+	create_user(shortname);	
+
