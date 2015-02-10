d2e672dd5b70de0754a5b907eecda14960a38ebb
Joshua Thorp <joshua@stigmergic.net>
2015-02-10 13:55:34 -0700

    make cache middleware less chatty

--- project/apps/utils/cache_headers_middleware.py
+++ project/apps/utils/cache_headers_middleware.py
@@ -19,13 +19,9 @@ class CachetimeLookup(object):
         
 
     def get_cache_time(self, path):
-        log.debug('matching path: {} '.format(path))
         for (pattern,timeout) in self.patterns:
-            log.debug('pattern: {}'.format(pattern.pattern))
             if pattern.match(path):
-                log.debug('path: {0} matched: {1} timeout: {2} '.format(path, pattern.pattern, timeout))
                 return timeout
-        log.debug('no match found')
         return self.default
 
 APP_DEFAULT_CACHE = getattr(settings, 'APP_DEFAULT_CACHE', 3600)
@@ -34,8 +30,8 @@ APP_DEFAULT_SHORT_CACHE = getattr(settings, 'APP_DEFAULT_SHORT_CACHE', 600)
 cache_lookup = CachetimeLookup(
                              default=getattr(settings, 'CACHE_MIDDLEWARE_SECONDS', 3601),
                              patterns = (
-                                         (u'/banner/', APP_DEFAULT_SHORT_CACHE),
-                                         (u'/company/(press|news)/.*', APP_DEFAULT_SHORT_CACHE),
+                                         (u'/default_short/', APP_DEFAULT_SHORT_CACHE),
+                                         (u'/app/(default|short)/.*', APP_DEFAULT_SHORT_CACHE),
                                          (u'/admin/.*', 0),
                                          (u'/', APP_DEFAULT_CACHE),
                                          )
@@ -51,13 +47,9 @@ class CacheHeadersMiddleware(object):
     """ overrides all Cache-Control settings on responses and injects headers from django.utils.cach.patch_response_headers """
 
     def process_response(self, request, response):
-        log.debug("host: {}".format(request.get_host()))
-        log.debug("headers: {}".format(get_headers(request)))
         if request.method in ['GET', 'HEAD']:
             path = request.path
             max_age = cache_lookup.get_cache_time(path)
-
-            log.debug( 'CACHING: {} max_age: {}'.format(path, max_age) )
 
             # dump current Cache-Control headers
             del response['Cache-Control']
