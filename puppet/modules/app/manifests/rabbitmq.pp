a284299111aad6424d4646e5b533b992dfa13bd7
Joshua Thorp <joshua@stigmergic.net>
2015-02-18 16:37:14 -0700

    rabbitmq install sets up service now

--- puppet/modules/app/manifests/rabbitmq.pp
+++ puppet/modules/app/manifests/rabbitmq.pp
@@ -1,12 +1,5 @@
-class app::rabbitmq{
+class app::rabbitmq {
     package{'rabbitmq-server':
         ensure => installed
-    }
-
-    service{ "rabbitmq-server":
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        require    => Package['rabbitmq-server']
     }
 }
