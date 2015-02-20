86b4bcb50657a4579c682e3467597acd7bd449dc
Joshua Thorp <joshua@stigmergic.net>
2015-02-20 10:33:53 -0700

    fix unicode logging issue,  then suppress logs,  belt and suspenders :crocodile:

--- backend/apps/utils/list_view_cache_middleware.py
+++ backend/apps/utils/list_view_cache_middleware.py
@@ -48,7 +48,7 @@ def update(response):
 
 class CacheGetListView(object):    
     def process_request(self, request):
-        log.debug('Method: {}  Path: {}'.format(request.method, request.path))
+#         log.debug(u'Method: {}  Path: {}'.format(request.method, request.path))
         if request.method not in ['GET', ]:
             return None
         
@@ -57,11 +57,11 @@ class CacheGetListView(object):
             response = cache.get(key_str, None)
 
             if response is not None:
-                log.debug('cache found... updating: {}'.format(key_str))
+#                 log.debug(u'cache found... updating: {}'.format(key_str))
                 response = update(response)
 
             if response is None:
-                log.debug('marking for save: {}'.format(key_str))
+#                 log.debug(u'marking for save: {}'.format(key_str))
                 request.cache_response = True
 
             return response
@@ -74,10 +74,10 @@ class CacheSetListView(object):
         key_str = cache_key_str(request)
         if check_key(key_str):
             if hasattr(request, 'cache_response'):
-                log.debug('setting cache for {} -- size: {}'.format(key_str, len(response.content)))
+#                 log.debug(u'setting cache for {} -- size: {}'.format(key_str, len(response.content)))
                 cache.set(key_str, response)
             else:
-                log.debug('skipping cache save: {}'.format(key_str))
-                
-        
+                pass
+#                 log.debug(u'skipping cache save: {}'.format(key_str))
+
         return response
