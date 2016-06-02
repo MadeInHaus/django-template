01457ba3db4845509c30838ed254ba39219d1654
Rafael <rafael@madeinhaus.com>
2016-06-02 15:46:03 -0300

    Set Docker variables before running commands on the container.

--- docker-manage.sh
+++ docker-manage.sh
@@ -4,4 +4,5 @@ CURRENT_DIR=${PWD##*/}
 dockerName=${CURRENT_DIR//[^a-zA-Z0-9]/}
 
 echo "Running \`python ./manage.py $*\` on web container for $dockerName"
+eval "$(docker-machine env default)"
 eval "docker-compose -p $dockerName -f docker-compose.dev.yml run web python ./manage.py $*"
