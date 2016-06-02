676f30ed54fc73eaf99a1219007b9d3d6edd9403
Rafael <rafael@madeinhaus.com>
2016-06-02 02:12:25 -0300

    Add shell script to run django commands on docker container.

---
+++ docker-manage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CURRENT_DIR=${PWD##*/}
+dockerName=${CURRENT_DIR//[^a-zA-Z0-9]/}
+
+echo "Running \`python ./manage.py $*\` on web container for $dockerName"
+eval "docker-compose -p $dockerName -f docker-compose.dev.yml run web python ./manage.py $*"
